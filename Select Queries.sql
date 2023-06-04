30218100d26b55984d45748490f36e37a5038b0f
William Nogueira <wdsn.dev@gmail.com>
2023-06-04 18:50:41 -0300

    adicao de novas queries

--- Select Queries.sql
+++ Select Queries.sql
@@ -1,19 +1,23 @@
-#Selecionar todos os funcionários do departamento de tecnologia:
+#Seleciona todos os funcionários do departamento de tecnologia:
 SELECT * FROM Funcionarios WHERE DepartamentoID = 5;
 
-#Selecionar todos os funcionários com salário acima de 5000,00 Reais:
+#Seleciona todos os funcionários com salário acima de 5000,00 Reais:
 SELECT * FROM Funcionarios WHERE Salario > 5000;
 
-#Selecionar todos os funcionários com seus respectivos departamentos:
+#Seleciona todos os funcionários com seus respectivos departamentos:
 SELECT F.*, D.Nome AS NomeDepartamento
 FROM Funcionarios F
 INNER JOIN Departamentos D ON F.DepartamentoID = D.DepartamentoID;
 
-#Selecionar todos os funcionários com seus endereços:
+#Seleciona todos os funcionários com seus endereços:
 SELECT F.*, E.Rua, E.Numero, E.Cidade, E.Estado, E.CEP
 FROM Funcionarios F
 INNER JOIN Enderecos E ON F.FuncionarioID = E.FuncionarioID;
 
-#Selecionar todos os dependentes dos funcionários com mais de 30 anos:
-SELECT * FROM Dependentes WHERE YEAR(CURDATE()) - YEAR(DataNascimento) > 30;
+#Seleciona todos os dependentes com mais de 12 anos:
+SELECT * FROM Dependentes WHERE YEAR(CURDATE()) - YEAR(DataNascimento) > 12;
 
+#Seleciona todos os funcionários com seus respectivos históricos de cargos:
+SELECT F.*, HC.Cargo, HC.DataInicio, HC.DataFim
+FROM Funcionarios F
+LEFT JOIN HistoricoCargos HC ON F.FuncionarioID = HC.FuncionarioID;
